b7b0f90a5de3253b63a3d9e3e1dcd274fc8020a1
Vladimir Parkhomchuk <vovaparkhomchuk@Vladimirs-MacBook-Air.local>
2020-02-10 12:22:34 +0200

    added prettier to v code

--- components/Timer.js
+++ components/Timer.js
@@ -1,44 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import TimerBtn from './TimerBtn'
-import { vibrate } from '../utils'
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import TimerBtn from "./TimerBtn";
+import { vibrate } from "../utils";
 
 const styles = StyleSheet.create({
   timerContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   bigFont: {
-    fontSize: 42,
+    fontSize: 42
   }
-})
-
+});
 
 const ActivityType = props => {
   if (!props.phaze) {
-    return (
-      <Text style={styles.bigFont}>Work Time</Text>
-    )
+    return <Text style={styles.bigFont}>Work Time</Text>;
   } else {
-    return (
-      <Text style={styles.bigFont}>Break Time</Text>
-    )
+    return <Text style={styles.bigFont}>Break Time</Text>;
   }
-}
-
+};
 
 export default class Timer extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       workT: 7,
       breakT: 5,
       currentTime: 0,
-      normcurrentTime: '',
+      normcurrentTime: "",
       timerOn: false,
-      secondPhaze: false,
-    }
+      secondPhaze: false
+    };
   }
 
   componentDidMount() {
@@ -48,74 +42,73 @@ export default class Timer extends React.Component {
   componentWillMount() {
     if (!this.state.secondPhaze) {
       this.setState({
-        currentTime: this.state.workT,
-      })
+        currentTime: this.state.workT
+      });
     }
   }
 
   decreaseTime = () => {
     this.setState(prevStates => ({
-      currentTime: prevStates.currentTime - 1,
-    }))
+      currentTime: prevStates.currentTime - 1
+    }));
     if (this.state.currentTime === 0 && !this.state.secondPhaze) {
-      vibrate()
+      vibrate();
       clearInterval(this.interval);
       this.setState({
         secondPhaze: true,
-        currentTime: this.state.breakT,
-      })
-      this.interval = setInterval(this.decreaseTime, 1000)
+        currentTime: this.state.breakT
+      });
+      this.interval = setInterval(this.decreaseTime, 1000);
     } else if (this.state.currentTime === 0 && this.state.secondPhaze) {
-      vibrate()
+      vibrate();
       clearInterval(this.interval);
       this.setState({
         timerOn: false,
         currentTime: this.state.workT,
-        secondPhaze: false,
-      })
+        secondPhaze: false
+      });
     }
     console.log(this.state.currentTime);
     this.correctTime();
-  }
+  };
 
   correctTime = () => {
     const workMins =
-      this.state.currentTime % 60 === 0 ? '00' :
-        (this.state.currentTime % 60).toString().length === 1 ? '0' + (this.state.currentTime % 60).toString() :
-          (this.state.currentTime % 60).toString();
+      this.state.currentTime % 60 === 0
+        ? "00"
+        : (this.state.currentTime % 60).toString().length === 1
+        ? "0" + (this.state.currentTime % 60).toString()
+        : (this.state.currentTime % 60).toString();
 
     this.setState(prevStates => ({
-      normcurrentTime: (Math.floor(prevStates.currentTime / 60)).toString() + ':' + workMins,
-    }))
+      normcurrentTime:
+        Math.floor(prevStates.currentTime / 60).toString() + ":" + workMins
+    }));
 
-    console.log((Math.floor(this.currentTime / 60)).toString() + ':' + workMins)
-  }
+    console.log(Math.floor(this.currentTime / 60).toString() + ":" + workMins);
+  };
 
   startTimer = () => {
     if (!this.state.timerOn) {
-      this.interval = setInterval(this.decreaseTime, 1000)
+      this.interval = setInterval(this.decreaseTime, 1000);
       this.setState(prevStates => ({
-        timerOn: !prevStates.timerOn,
-      }))
+        timerOn: !prevStates.timerOn
+      }));
     } else {
-      clearInterval(this.interval)
+      clearInterval(this.interval);
       this.setState(prevStates => ({
-        timerOn: !prevStates.timerOn,
-      }))
+        timerOn: !prevStates.timerOn
+      }));
     }
-  }
-
+  };
 
   render() {
     return (
       <View style={styles.timerContainer}>
         <ActivityType phaze={this.state.secondPhaze} />
-        <Text style={styles.bigFont}>
-          {this.state.normcurrentTime}
-        </Text>
+        <Text style={styles.bigFont}>{this.state.normcurrentTime}</Text>
         <TimerBtn timerState={this.state.timerOn} timerFunc={this.startTimer} />
       </View>
-    )
+    );
   }
 }
-
--- components/TimerBtn.js
+++ components/TimerBtn.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import { Button } from 'react-native'
+import React from "react";
+import { Button } from "react-native";
 
 export default class TimerBtn extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
   render() {
     if (this.props.timerState) {
-      return (
-        <Button title="stop" onPress={this.props.timerFunc} />
-      )
+      return <Button title="stop" onPress={this.props.timerFunc} />;
     } else {
-      return (
-        <Button title="start" onPress={this.props.timerFunc} />
-      )
+      return <Button title="start" onPress={this.props.timerFunc} />;
     }
   }
 }
